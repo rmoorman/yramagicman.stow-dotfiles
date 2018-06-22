a22327dc345ff7a36bb113c7c2e7e05cea8c9d2f
Jonathan <jonathandavis@gilsons.org>
2018-06-22 15:50:06 -0400

    better formatting

--- init.sh
+++ init.sh
@@ -6,32 +6,30 @@ then
     then
         echo "On Arch, installing stow automatically"
         sudo pacman -S stow
-        else
-            echo "Please install GNU Stow"
-
-            return
-        fi
     else
-        echo ''
+        echo "Please install GNU Stow"
+
+        return
     fi
+fi
 
-    find ~/ -maxdepth 1 -type f -iname '.*' | while read -r f
-    do
-        if test -f "./root/$(basename "$f")"
-        then
-            echo "Backing up $f"
-            mv -v "$f" "$f.bak"
-        fi
-    done
-
-    for f in $(find ./ -maxdepth 1 -type d | grep -Ev '.git$|config|zsh' | cut -d '/' -f 2 )
-    do
-        stow -R "$f" -t ~/
-    done
-
-    if test ! -d "$HOME/.config"
+find ~/ -maxdepth 1 -type f -iname '.*' | while read -r f
+do
+    if test -f "./root/$(basename "$f")"
     then
-        echo "making ~/.config directory"
-        mkdir -p "$HOME/.config"
+        echo "Backing up $f"
+        mv -v "$f" "$f.bak"
     fi
-    stow -R config -t ~/.config
+done
+
+for f in $(find ./ -maxdepth 1 -type d | grep -Ev '.git$|config|zsh' | cut -d '/' -f 2 )
+do
+    stow -R "$f" -t ~/
+done
+
+if test ! -d "$HOME/.config"
+then
+    echo "making ~/.config directory"
+    mkdir -p "$HOME/.config"
+fi
+stow -R config -t ~/.config
