971cbd7ca4ab1c688dbe85d8326ea1309894cf56
Jonathan <jonathandavis@gilsons.org>
2018-12-08 22:06:47 -0500

    archinstall script

--- bin/setup/archinstall.sh
+++ bin/setup/archinstall.sh
@@ -1,27 +1,30 @@
 set -e
 partition() {
     if test -n "$(ls /sys/firmware/efi/efivars)"; then
-	parted "$1" mkpart primary fat32 1MiB 551MiB
-	parted "$1" set 1 esp on
-	parted "$1" mkpart primary ext4 551MiB $2
-	parted "$1" mkpart primary ext4 $2 100%
-	mkfs.vfat "$1""1"
-	mkfs.ext4 "$1""2"
-	mkfs.ext4 "$1""3"
+        parted "$1" mkpart primary fat32 1MiB 551MiB
+        parted "$1" set 1 esp on
+        parted "$1" mkpart primary ext4 551MiB $2
+        parted "$1" mkpart primary ext4 $2 100%
+        mkfs.vfat "$1""1"
+        mkfs.ext4 "$1""2"
+        mkfs.ext4 "$1""3"
     else
-	parted $1 mkpart primary ext4 1MiB $2
-	parted $1 set 1 boot on
-	parted $1 mkpart primary ext4 $2 100%
-	mkfs.ext4 "$1""1"
-	mkfs.ext4 "$1""2"
+        parted $1 mkpart primary ext4 1MiB $2
+        parted $1 set 1 boot on
+        parted $1 mkpart primary ext4 $2 100%
+        mkfs.ext4 "$1""1"
+        mkfs.ext4 "$1""2"
     fi
 }
 
+mnt_drives() {
+
+}
 if ! curl https://example.com > /dev/null; then
     wifi-menu
     if ! curl https://example.com; then
-	printf 'no internet. please reconnect and try again'
-	exit
+        printf 'no internet. please reconnect and try again'
+        exit
     fi
 fi
 timedatectl set-ntp true
