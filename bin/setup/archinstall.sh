bc3daba9db4cd546996706ca1cc0854b0d6994e1
Jonathan <jonathandavis@gilsons.org>
2018-12-08 22:54:06 -0500

    I cant figure out what this is doing

--- bin/setup/archinstall.sh
+++ bin/setup/archinstall.sh
@@ -1,18 +1,18 @@
 partition() {
     if test -n "$(ls /sys/firmware/efi/efivars)"; then
-        echo parted "$1" mkpart primary fat32 1MiB 551MiB
-        echo parted "$1" set 1 esp on
-        echo parted "$1" mkpart primary ext4 551MiB $2
-        echo parted "$1" mkpart primary ext4 $2 100%
-        echo mkfs.vfat "$1""1"
-        echo mkfs.ext4 "$1""2"
-        echo mkfs.ext4 "$1""3"
+        echo "parted $1 mkpart primary fat32 1MiB 551MiB"
+        echo "parted $1 set 1 esp on"
+        echo "parted $1 mkpart primary ext4 551MiB $2"
+        echo "parted $1 mkpart primary ext4 $2 100%"
+        echo "mkfs.vfat $1 1"
+        echo "mkfs.ext4 $1 2"
+        echo "mkfs.ext4 $1 3"
     else
-        echo parted "$1" mkpart primary ext4 1MiB $2
-        echo parted "$1" set 1 boot on
-        echo parted "$1" mkpart primary ext4 $2 100%
-        echo mkfs.ext4 "$1""1"
-        echo mkfs.ext4 "$1""2"
+        echo "parted $1 mkpart primary ext4 1MiB $2"
+        echo "parted $1 set 1 boot on"
+        echo "parted $1 mkpart primary ext4 $2 100%"
+        echo "mkfs.ext4 $11"
+        echo "mkfs.ext4 $12"
     fi
 }
 
