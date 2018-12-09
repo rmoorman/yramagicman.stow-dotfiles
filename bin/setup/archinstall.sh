0a1ccac5a83d6b7e781dd98387782aa532f9e0bd
Jonathan <jonathandavis@gilsons.org>
2018-12-09 09:49:07 -0500

    I wasnt createing a partition table

--- bin/setup/archinstall.sh
+++ bin/setup/archinstall.sh
@@ -3,6 +3,7 @@
 partition() {
     if [ -d /sys/firmware/efi/efivars ]
     then
+        echo "parted $1 mklabel gpt"
         echo "parted $1 mkpart primary fat32 1MiB 551MiB"
         echo "parted $1 set 1 esp on"
         echo "parted $1 mkpart primary ext4 551MiB $2"
@@ -11,11 +12,12 @@ partition() {
         echo "mkfs.ext4 $1 2"
         echo "mkfs.ext4 $1 3"
     else
-         parted $1 mkpart primary ext4 1MiB "$2"
-         parted $1 set 1 boot on
-         parted $1 mkpart primary ext4 "$2" 100%
-         echo mkfs.ext4 "$1""1"
-         echo mkfs.ext4 "$1""2"
+        parted "$1" mklabel msdos
+        parted "$1" mkpart primary ext4 1MiB "$2"
+        parted "$1" set 1 boot on
+        parted "$1" mkpart primary ext4 "$2" 100%
+        echo mkfs.ext4 "$1""1"
+        echo mkfs.ext4 "$1""2"
     fi
 }
 
