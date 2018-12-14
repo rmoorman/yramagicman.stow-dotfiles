4c87c3c4df39b71af096841b023e225569b88076
Jonathan <jonathandavis@gilsons.org>
2018-12-13 21:45:54 -0500

    fix locale-gen command

--- bin/setup/archinstall_2.sh
+++ bin/setup/archinstall_2.sh
@@ -2,7 +2,7 @@
 ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
 hwclock --systohc
 sed -i 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
-local-gen
+locale-gen
 echo 'LANG=en_US.UTF-8' > /etc/locale.conf
 
 read -r hstname
