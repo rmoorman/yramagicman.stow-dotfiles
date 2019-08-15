acffb28af4e1994892de2a469f36af108430815e
Jonathan <jonathandavis@gilsons.org>
2019-08-14 20:46:43 -0400

    update .emacs

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -18,15 +18,26 @@
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
-(add-to-list 'load-path
-	     (expand-file-name "use-package" user-emacs-directory))
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
 
 (require 'use-package)
 (require 'use-package-ensure)
 (setq use-package-always-ensure t)
 (use-package evil
   :config
-  (evil-mode 1))
+  (evil-mode 1)
+  (add-hook 'evil-normal-state-entry-hook (lambda ()
+					    (if buffer-file-name
+						(when 
+						    (file-writable-p buffer-file-name)
+						  (save-buffer)))
+					    (lambda() nil))))
+
 (use-package evil-escape
   :init
   (setq-default evil-escape-key-sequence "  ")
@@ -35,16 +46,31 @@
   (evil-escape-mode 1))
 (use-package evil-leader
   :config
-  (evil-leader-mode 1))
+  (evil-leader/set-leader ",")
+  (evil-leader/set-key
+    "," 'evil-switch-to-windows-last-buffer)
+  (global-evil-leader-mode 1))
 (use-package auto-complete
   :config
-  (auto-complete-mode 1))
+  (global-auto-complete-mode 1))
 (use-package php-mode
   :mode "\\.php\\'"
   :interpreter "php")
+(use-package ac-php
+  :mode "\\.php\\'"
+  :interpreter "php")
+
 (use-package markdown-mode
   :mode "\\.md\\'")
+(use-package linum-relative
+  :config
+  (linum-relative-global-mode))
+
+(use-package vterm)
+(use-package magit)
+
 (ido-mode 1)
+(show-paren-mode 1)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -53,8 +79,7 @@
  '(electric-pair-mode t)
  '(package-selected-packages
    (quote
-    (markdown-mode evil-leader php-mode auto-complete evil-escape undo-tree evil)))
- '(tab-always-indent nil))
+    (magit ac-php vterm linum-relative shell-script-mode use-package markdown-mode evil-leader php-mode auto-complete evil-escape undo-tree evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
