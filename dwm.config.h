b3be2e69f58df2b072ff686460e9c2b8157cca20
Jonathan <jonathandavis@gilsons.org>
2020-02-22 00:17:22 -0500

    update rice

--- dwm.config.h
+++ dwm.config.h
@@ -3,16 +3,16 @@
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 0;        /* 0 means no bar */
-static const int topbar             = 0;        /* 0 means bottom bar */
+static const int showbar            = 1;        /* 0 means no bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Inconsolata:size=11" };
 static const char dmenufont[]       = "Inconsolata:size=12";
 
 static const char border[] = "#2b303b";
-static const char sel_border[] = "#bf616a";
+static const char sel_border[] = "#8fa1b3";
 
 static const char background[] = "#2b303b";
-static const char sel_background[] = "#bf616a";
+static const char sel_background[] = "#8fa1b3";
 
 static const char foreground[] = "#c0c5ce";
 static const char sel_foreground[] = "#2b303b";
