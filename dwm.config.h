99ff1d8410d69da7203531541c885220cdb179eb
Jonathan <jonathandavis@gilsons.org>
2020-03-22 22:17:19 -0400

    temporary fix

--- dwm.config.h
+++ dwm.config.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Inconsolata:size=11" };
-static const char dmenufont[]       = "Inconsolata:size=12";
+static const char *fonts[]          = { "mono:size=9.5" };
+static const char dmenufont[]       = "mono:size=10";
 
 static const char border[] = "#2b303b";
 static const char sel_border[] = "#8fa1b3";
