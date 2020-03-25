e57301cb25268ad6eaf18dfbdc150fcb0f7138d4
Jonathan <jonathandavis@gilsons.org>
2020-03-25 12:38:42 -0400

    restructure cron, refactor init.sh

--- init.sh
+++ init.sh
@@ -1,48 +1,56 @@
 #!/bin/sh
 logfile=$HOME/initlog
-printf '' > "$logfile"
 dotdir="$(dirname "$(realpath "$0")" )"
-find ./ -maxdepth 1 -type f  \
-    -not -name '.*' -and \
-    -not -name 'config' -and \
-    -not -name 'license.txt' -and \
-    -not -name '*.h' -and \
-    -not -name '*cron' -and \
-    -not -name 'README.md' -and \
-    -not -name 'uninstall.sh' -and \
-    -not -name 'init.sh' | while read -r f
-do
-    if test ! -f "$HOME/.$(basename "$f" )"
+reposdir="$HOME/Gits"
+printf '' > "$logfile"
+dotfiles() {
+    find ./ -maxdepth 1 -type f  \
+        -not -name '.*' -and \
+        -not -name 'config' -and \
+        -not -name 'license.txt' -and \
+        -not -name '*.h' -and \
+        -not -name 'cron' -and \
+        -not -name 'README.md' -and \
+        -not -name 'uninstall.sh' -and \
+        -not -name 'init.sh' | while read -r f
+        do
+            if test ! -f "$HOME/.$(basename "$f" )"
+            then
+                ln -sfv "$dotdir/$(basename "$f")" "$HOME/.$(basename "$f")"
+            else
+                printf 'Files Not Installed: ' >> "$logfile"
+                printf "%s\n" "$f" >> "$logfile"
+
+            fi
+        done
+    }
+
+dotdirs() {
+    find ./ -maxdepth 1 -type d  \
+        -not -name '.git' -and \
+        -not -name 'config' -and \
+        -not -name 'cron' -and \
+        -not -name 'githooks' -and \
+        -not -name 'bin' | while read -r d
+        do
+            if test ! -d "$HOME/.$(basename "$d" )"
+            then
+                ln -sv "$dotdir/$(basename "$d")" "$HOME/.$(basename "$d")"
+            else
+                printf '\nDot Directories Not Installed: ' >> "$logfile"
+                printf "%s\n" "$d" >> "$logfile"
+
+            fi
+        done
+    }
+
+configdir() {
+    if test ! -d "$HOME/.config"
     then
-        ln -sf "$dotdir/$(basename "$f")" "$HOME/.$(basename "$f")"
-    else
-        printf 'Files Not Installed: ' >> "$logfile"
-        printf "%s\n" "$f" >> "$logfile"
-
+        echo "making ~/.config directory"
+        mkdir -p "$HOME/.config"
     fi
-done
-find ./ -maxdepth 1 -type d  \
-    -not -name '.git' -and \
-    -not -name 'config' -and \
-    -not -name 'githooks' -and \
-    -not -name 'bin' | while read -r d
-do
-    if test ! -d "$HOME/.$(basename "$d" )"
-    then
-        ln -sv "$dotdir/$(basename "$d")" "$HOME/.$(basename "$d")"
-    else
-        printf '\nDot Directories Not Installed: ' >> "$logfile"
-        printf "%s\n" "$d" >> "$logfile"
-
-    fi
-done
-
-if test ! -d "$HOME/.config"
-then
-    echo "making ~/.config directory"
-    mkdir -p "$HOME/.config"
-fi
-find "$PWD/config/" -maxdepth 1 -not -name 'config' | while read -r directory
+    find "$dotdir/config/" -maxdepth 1 -not -name 'config' | while read -r directory
 do
     if test ! -d "$HOME/.config/$(basename "$directory" )" && test ! -f "$HOME/.config/$(basename "$directory")"
     then
@@ -53,65 +61,161 @@ do
 
     fi
 done
+}
 
-for f in githooks/*; do
-    ln -sf "$dotdir/$f" "$dotdir/.git/hooks/$(basename "$f")"
-done
+githooks() {
+    for f in githooks/*; do
+        ln -sf "$dotdir/$f" "$dotdir/.git/hooks/$(basename "$f")"
+    done
+}
 
-if test ! -d "$HOME/bin"
-then
-    ln -sv "$dotdir/bin" "$HOME/"
-else
-    printf '\nMisc files Not Installed: ' >> "$logfile"
-    printf "%s\n" "$dotdir/bin" >> "$logfile"
-fi
-
-if test ! -d "$HOME/Gits"
-then
-    mkdir -p "$HOME/Gits"
-fi
-if test "$(uname)" != "Darwin"
-then
-    if test ! -d "$HOME/Gits/st/"
+bindir() {
+    if test ! -d "$HOME/bin"
     then
+        ln -sv "$dotdir/bin" "$HOME/"
+    else
+        printf '\nMisc files Not Installed: ' >> "$logfile"
+        printf "%s\n" "$dotdir/bin" >> "$logfile"
+    fi
+}
+
+mkrepodir() {
+    if test ! -d "$reposdir"
+    then
+        mkdir -p "$reposdir"
+    fi
+}
+
+st() {
+    if test "$(uname)" != "Darwin"
+    then
+        if test ! -d "$HOME/Gits/st/"
+        then
+            (
+            cd "$HOME/Gits" || return
+            git clone --depth 3 git://git.suckless.org/st
+            cp "$dotdir/st.config.h" "$HOME/Gits/st/config.h"
+            cd "$HOME/Gits/st" || return
+            make
+            sudo make install
+        )
+    else
         (
-        cd "$HOME/Gits" || return
-        git clone --depth 3 git://git.suckless.org/st
         cp "$dotdir/st.config.h" "$HOME/Gits/st/config.h"
         cd "$HOME/Gits/st" || return
         make
-        sudo make install
     )
-else
-    (
-    cp "$dotdir/st.config.h" "$HOME/Gits/st/config.h"
-    cd "$HOME/Gits/st" || return
-    make
-)
+        fi
     fi
-    if test ! -d "$HOME/Gits/dwm/"
+}
+
+dwm() {
+    if test "$(uname)" != "Darwin"
     then
+        if test ! -d "$HOME/Gits/dwm/"
+        then
+            (
+            cd "$HOME/Gits" || return
+            git clone --depth 3 git://git.suckless.org/dwm
+            cp "$dotdir/dwm.config.h" "$HOME/Gits/dwm/config.h"
+            cd "$HOME/Gits/dwm" || return
+            make
+            sudo make install
+        )
+    else
         (
-        cd "$HOME/Gits" || return
-        git clone --depth 3 git://git.suckless.org/dwm
         cp "$dotdir/dwm.config.h" "$HOME/Gits/dwm/config.h"
         cd "$HOME/Gits/dwm" || return
         make
-        sudo make install
     )
-else
-    (
-    cp "$dotdir/dwm.config.h" "$HOME/Gits/dwm/config.h"
-    cd "$HOME/Gits/dwm" || return
-    make
-)
+        fi
     fi
-fi
-if test "$SHELL" != "$(command -v zsh)"; then
-    chsh -s "$(command -v zsh)"
-fi
+}
 
-printf "installing cronjobs for user\n"
-crontab "$dotdir/joncron"
-printf "installing cronjobs for root\n"
-sudo crontab "$dotdir/rootcron"
+setshell() {
+    if test "$SHELL" != "$(command -v zsh)"; then
+        chsh -s "$(command -v zsh)"
+    fi
+}
+
+cron() {
+    find "$dotdir/cron" -type f | while read -r job
+do
+    j="$(basename "$job" )"
+    if test "$j" == 'root'
+    then
+        echo sudo crontab "$job"
+    else
+        if test "$(who | cut -d ' ' -f 1 | sed 1q)" == "$j"
+        then
+            echo crontab -u "$j" "$job"
+        fi
+    fi
+done
+}
+
+args="$(getopt fdcbrtwsja "$@")"
+if test -z "$1"
+then
+    cat <<EOF
+-f) Dot files
+-d) Dot directoriess
+-c) Config directories
+-b) Bin directory
+-r) Make repos directory
+-t) Build st
+-w) Build dwm
+-s) Set shell
+-j) Install cron jobs
+-a) Installs everything
+EOF
+fi
+set -- $args
+for i
+do
+    case "$i"
+        in
+    -f)
+        dotfiles
+        shift;;
+    -d)
+        dotdirs
+        shift;;
+    -c)
+        configdir
+        shift;;
+    -b)
+        bindir
+        shift;;
+    -r)
+        mkrepodir
+        shift;;
+    -t)
+        st
+        shift;;
+    -w)
+        dwm
+        shift;;
+    -s)
+        setshell
+        shift;;
+    -j)
+        cron
+        shift;;
+    -a)
+        bindir
+        configdir
+        cron
+        dotdirs
+        dotfiles
+        dwm
+        mkrepodir
+        setshell
+        st
+        shift;;
+    --)
+        githooks
+        shift;
+        break;;
+esac
+done
