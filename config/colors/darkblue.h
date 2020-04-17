b5107d3de9c2167ba73ea8c734f0312f645da576
Jonathan <jonathandavis@gilsons.org>
2020-04-17 13:56:53 -0400

    minor updates

--- config/colors/darkblue.h
+++ config/colors/darkblue.h
@@ -2,28 +2,28 @@
 static const char *colorname[] = {
 
   /* 8 normal colors */
-  [0] = "#113457", /* black   */
-  [1] = "#cb3f3f", /* red     */
+  [0] = "#0a1f34", /* black   */
+  [1] = "#7f0000", /* red     */
   [2] = "#409444", /* green   */
   [3] = "#bdab00", /* yellow  */
   [4] = "#246ca7", /* blue    */
   [5] = "#6e3e7e", /* magenta */
   [6] = "#0f9595", /* cyan    */
-  [7] = "#696969", /* white   */
+  [7] = "#727272", /* white   */
 
   /* 8 bright colors */
   [8]  = "#114780", /* black   */
-  [9]  = "#ff9393", /* red     */
+  [9]  = "#cd3737", /* red     */
   [10] = "#51bb56", /* green   */
   [11] = "#e1cc00", /* yellow  */
   [12] = "#56a1df", /* blue    */
   [13] = "#b374c5", /* magenta */
   [14] = "#51b2b2", /* cyan    */
-  [15] = "#b7bab8", /* white   */
+  [15] = "#b5b5b5", /* white   */
 
   /* special colors */
   [256] = "#001428", /* background */
-  [257] = "#c5c8c6", /* foreground */
+  [257] = "#b5b5b5", /* foreground */
 };
 
 /*
