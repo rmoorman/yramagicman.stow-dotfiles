04d36a85429ce6462bdcee91cb982ad1afd28bb0
Jonathan <jonathandavis@gilsons.org>
2020-05-18 20:58:47 -0400

    I think need this anyway

---
+++ config/emacs/custom.el
@@ -0,0 +1,24 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#262626" "#d75f5f" "#afaf00" "#ffaf00" "#83adad" "#d485ad" "#83adad" "#dab997"])
+ '(custom-enabled-themes '(base16-gruvbox-dark-pale))
+ '(custom-file "~/.config/emacs/custom.el")
+ '(custom-safe-themes
+   '("50d07ab55e2b5322b2a8b13bc15ddf76d7f5985268833762c500a90e2a09e7aa" default))
+ '(flycheck-typescript-tslint-executable "~/.local/bin/tslint")
+ '(package-selected-packages
+   '(lsp-mode base16-theme magit flycheck typescript-mode web-mode linum-relative racket-mode markdown-mode ivy lua-mode ac-php auto-complete evil-surround evil-commentary general evil-escape evil-collection evil projectile use-package))
+ '(projectile-enable-caching t)
+ '(tab-width 4))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(hl-line ((t (:extend t :background "#3a3a3a")))))
+(provide 'custom.el)
+;;; custom.el ends here
