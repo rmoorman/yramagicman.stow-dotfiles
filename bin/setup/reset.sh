43afe45cae2db8ce5e5d1b96f0f48b4ab0f34458
Jonathan <jonathandavis@gilsons.org>
2020-05-27 13:30:32 -0400

    fix package list

--- bin/setup/reset.sh
+++ bin/setup/reset.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 pacman -D --asdeps $(pacman -Qqe)
-pacman -D --asexplicit base base-devel linux linux-firmware zsh vim
+pacman -D --asexplicit base base-devel linux linux-firmware zsh
 pacman -Rns $(pacman -Qtdq)
