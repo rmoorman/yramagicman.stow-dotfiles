7d057abb26b36df8d206b538d0b935b72fda5320
Jonathan <jonathandavis@gilsons.org>
2020-05-27 14:43:27 -0400

    maybe xmobar will work on my other machine now

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -53,98 +53,98 @@ myFocusedBorderColor = "#ff0000"
 ------------------------------------------------------------------------
 -- Key bindings. Add, modify or remove key bindings here.
 --
-myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
+--myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
 
-    -- launch a terminal
-    [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
+--    -- launch a terminal
+--    [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
 
-    -- launch dmenu
-    , ((modm,               xK_p     ), spawn "dmenu_run")
+--    -- launch dmenu
+--    , ((modm,               xK_p     ), spawn "dmenu_run")
 
-    -- launch gmrun
-    , ((modm .|. shiftMask, xK_p     ), spawn "gmrun")
+--    -- launch gmrun
+--    , ((modm .|. shiftMask, xK_p     ), spawn "gmrun")
 
-    -- close focused window
-    , ((modm .|. shiftMask, xK_c     ), kill)
+--    -- close focused window
+--    , ((modm .|. shiftMask, xK_c     ), kill)
 
-     -- Rotate through the available layout algorithms
-    , ((modm,               xK_space ), sendMessage NextLayout)
+--     -- Rotate through the available layout algorithms
+--    , ((modm,               xK_space ), sendMessage NextLayout)
 
-    --  Reset the layouts on the current workspace to default
-    , ((modm .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
+--    --  Reset the layouts on the current workspace to default
+--    , ((modm .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
 
-    -- Resize viewed windows to the correct size
-    , ((modm,               xK_n     ), refresh)
+--    -- Resize viewed windows to the correct size
+--    , ((modm,               xK_n     ), refresh)
 
-    -- Move focus to the next window
-    , ((modm,               xK_Tab   ), windows W.focusDown)
+--    -- Move focus to the next window
+--    , ((modm,               xK_Tab   ), windows W.focusDown)
 
-    -- Move focus to the next window
-    , ((modm,               xK_j     ), windows W.focusDown)
+--    -- Move focus to the next window
+--    , ((modm,               xK_j     ), windows W.focusDown)
 
-    -- Move focus to the previous window
-    , ((modm,               xK_k     ), windows W.focusUp  )
+--    -- Move focus to the previous window
+--    , ((modm,               xK_k     ), windows W.focusUp  )
 
-    -- Move focus to the master window
-    , ((modm,               xK_m     ), windows W.focusMaster  )
+--    -- Move focus to the master window
+--    , ((modm,               xK_m     ), windows W.focusMaster  )
 
-    -- Swap the focused window and the master window
-    , ((modm,               xK_Return), windows W.swapMaster)
+--    -- Swap the focused window and the master window
+--    , ((modm,               xK_Return), windows W.swapMaster)
 
-    -- Swap the focused window with the next window
-    , ((modm .|. shiftMask, xK_j     ), windows W.swapDown  )
+--    -- Swap the focused window with the next window
+--    , ((modm .|. shiftMask, xK_j     ), windows W.swapDown  )
 
-    -- Swap the focused window with the previous window
-    , ((modm .|. shiftMask, xK_k     ), windows W.swapUp    )
+--    -- Swap the focused window with the previous window
+--    , ((modm .|. shiftMask, xK_k     ), windows W.swapUp    )
 
-    -- Shrink the master area
-    , ((modm,               xK_h     ), sendMessage Shrink)
+--    -- Shrink the master area
+--    , ((modm,               xK_h     ), sendMessage Shrink)
 
-    -- Expand the master area
-    , ((modm,               xK_l     ), sendMessage Expand)
+--    -- Expand the master area
+--    , ((modm,               xK_l     ), sendMessage Expand)
 
-    -- Push window back into tiling
-    , ((modm,               xK_t     ), withFocused $ windows . W.sink)
+--    -- Push window back into tiling
+--    , ((modm,               xK_t     ), withFocused $ windows . W.sink)
 
-    -- Increment the number of windows in the master area
-    , ((modm              , xK_comma ), sendMessage (IncMasterN 1))
+--    -- Increment the number of windows in the master area
+--    , ((modm              , xK_comma ), sendMessage (IncMasterN 1))
 
-    -- Deincrement the number of windows in the master area
-    , ((modm              , xK_period), sendMessage (IncMasterN (-1)))
+--    -- Deincrement the number of windows in the master area
+--    , ((modm              , xK_period), sendMessage (IncMasterN (-1)))
 
-    -- Toggle the status bar gap
-    -- Use this binding with avoidStruts from Hooks.ManageDocks.
-    -- See also the statusBar function from Hooks.DynamicLog.
-    --
-    -- , ((modm              , xK_b     ), sendMessage ToggleStruts)
+--    -- Toggle the status bar gap
+--    -- Use this binding with avoidStruts from Hooks.ManageDocks.
+--    -- See also the statusBar function from Hooks.DynamicLog.
+--    --
+--    -- , ((modm              , xK_b     ), sendMessage ToggleStruts)
 
-    -- Quit xmonad
-    , ((modm .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
+--    -- Quit xmonad
+--    , ((modm .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
 
-    -- Restart xmonad
-    , ((modm              , xK_q     ), spawn "xmonad --recompile; xmonad --restart")
+--    -- Restart xmonad
+--    , ((modm              , xK_q     ), spawn "xmonad --recompile; xmonad --restart")
 
-    -- Run xmessage with a summary of the default keybindings (useful for beginners)
-    , ((modm .|. shiftMask, xK_slash ), spawn ("echo \"" ++ help ++ "\" | xmessage -file -"))
-    ]
-    ++
+--    -- Run xmessage with a summary of the default keybindings (useful for beginners)
+--    , ((modm .|. shiftMask, xK_slash ), spawn ("echo \"" ++ help ++ "\" | xmessage -file -"))
+--    ]
+--    ++
 
-    --
-    -- mod-[1..9], Switch to workspace N
-    -- mod-shift-[1..9], Move client to workspace N
-    --
-    [((m .|. modm, k), windows $ f i)
-        | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
-        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
-    ++
+--    --
+--    -- mod-[1..9], Switch to workspace N
+--    -- mod-shift-[1..9], Move client to workspace N
+--    --
+--    [((m .|. modm, k), windows $ f i)
+--        | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
+--        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+--    ++
 
-    --
-    -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
-    -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
-    --
-    [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
-        | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
-        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+--    --
+--    -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
+--    -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
+--    --
+--    [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
+--        | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
+--        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
 
 
 ------------------------------------------------------------------------
@@ -209,7 +209,7 @@ myManageHook = composeAll
 -- return (All True) if the default handler is to be run afterwards. To
 -- combine event hooks use mappend or mconcat from Data.Monoid.
 --
-myEventHook = mempty
+-- myEventHook = mempty
 
 -- Startup hook
 
@@ -242,6 +242,9 @@ main = do
         , ((0, xK_Print), spawn "scrot")
         , ((shiftMask, xK_F12), spawn "systemctl poweroff")
         , ((mod4Mask, xK_m), spawn "firefox")
+        , ((mod4Mask, xK_q     ), kill)
+        , ((mod4Mask    .|. controlMask, xK_q     ), spawn "xmonad --recompile; xmonad --restart")
+        , ((mod4Mask              , xK_b     ), sendMessage ToggleStruts)
         ]
 -- A structure containing your configuration settings, overriding
 -- fields in the default config. Any you don't override, will
@@ -261,13 +264,13 @@ defaults = def {
         focusedBorderColor = myFocusedBorderColor,
 
       -- key bindings
-        keys               = myKeys,
+        -- keys               = myKeys,
         mouseBindings      = myMouseBindings,
 
       -- hooks, layouts
         layoutHook         = myLayout,
         manageHook         = myManageHook,
-        handleEventHook    = myEventHook,
+        -- handleEventHook    = myEventHook,
 
         startupHook        = myStartupHook
     }
