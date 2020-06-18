4214b306781480e72015b7b068e9f673c686f0d8
Jonathan <jonathandavis@gilsons.org>
2020-06-18 14:37:22 -0400

    hopefully don't turn internal display off when external displays are disconnected

--- config/screenlayout/work.sh
+++ config/screenlayout/work.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-xrandr --output eDP1 --off --output DP1 --off --output DP1-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1-2 --mode 1920x1080 --pos 1921x0 --rotate normal --output DP1-3 --off --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off
+if test "$(xrandr | grep -c 'connected')" -gt 1; then
+    xrandr --output eDP1 --off --output DP1 --off --output DP1-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1-2 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP1-3 --off --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off
+fi
