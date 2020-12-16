7a4da174ea16ff7b03d1d7d416bd2a4b3124455c
Jonathan <jonathandavis@gilsons.org>
2020-12-16 11:03:57 -0500

    move/copy to current directory if destination not provided

--- config/zsh/aliases/aliases.zsh
+++ config/zsh/aliases/aliases.zsh
@@ -45,9 +45,9 @@ function vimrc() {
 }
 #}}}
 #{{{ always recursive and verbose
-alias cp="cp -rv"
+# alias cp="cp -rv"
 alias rm="rm -rv"
-alias mv='mv -v'
+# alias mv='mv -v'
 #}}}
 #{{{ web dev stuff
 alias speedtest="wget -O /dev/null \
