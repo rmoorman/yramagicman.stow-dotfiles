438dc48114969825c650c4f07104aa0b30d45986
Jonathan <jonathandavis@gilsons.org>
2021-06-24 15:45:13 -0400

    use default modeline for now

--- config/emacs/lisp/my_modeline.el
+++ config/emacs/lisp/my_modeline.el
@@ -3,35 +3,35 @@
 ;;; Code:
 
 
-(setq-default mode-line-format (list
-                                " "
-                                mode-line-client
-                                '(:eval
-                                  (propertize
-                                   (cond ((eq 'emacs evil-state) " EMACS " )
-                                         ((eq 'normal evil-state) " NORMAL ")
-                                         ((eq 'visual evil-state) " VISUAL ")
-                                         ((eq 'insert evil-state) " INSERT ")
-                                         (t " U " )
-                                         )))
-                                " %& "
-                                vc-mode
-                                " %m "
-                                '(:eval
-                                  (propertize
-                                   (if (buffer-file-name)
-                                       (mapconcat 'identity (nthcdr 4 (split-string (buffer-file-name) "/")) "/")
-                                     " ")))
+;; (setq-default mode-line-format (list
+;;                                 " "
+;;                                 mode-line-client
+;;                                 '(:eval
+;;                                   (propertize
+;;                                    (cond ((eq 'emacs evil-state) " EMACS " )
+;;                                          ((eq 'normal evil-state) " NORMAL ")
+;;                                          ((eq 'visual evil-state) " VISUAL ")
+;;                                          ((eq 'insert evil-state) " INSERT ")
+;;                                          (t " U " )
+;;                                          )))
+;;                                 " %& "
+;;                                 vc-mode
+;;                                 " %m "
+;;                                 '(:eval
+;;                                   (propertize
+;;                                    (if (buffer-file-name)
+;;                                        (mapconcat 'identity (nthcdr 4 (split-string (buffer-file-name) "/")) "/")
+;;                                      " ")))
 
 
 
-                                " "
-                                mode-line-percent-position
-                                " "
-                                " L:%l "
+;;                                 " "
+;;                                 mode-line-percent-position
+;;                                 " "
+;;                                 " L:%l "
 
-                                ))
+;;                                 ))
 
-(force-mode-line-update)
+; (force-mode-line-update)
 (provide 'my_modeline)
 ;;; modeline.el ends here
