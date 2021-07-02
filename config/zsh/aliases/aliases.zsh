624220e5e8d51e174f02f06546ae2b05c7d08f84
Jonathan <jonathandavis@gilsons.org>
2021-07-02 17:59:54 -0400

    minor updates

--- config/zsh/aliases/aliases.zsh
+++ config/zsh/aliases/aliases.zsh
@@ -7,8 +7,8 @@ alias ~="cd ~"
 alias d="dirs -v"
 #}}}
 #{{{ shortcuts
-alias mutt="mutt -F $HOME/.config/mutt/muttrc && mbsync -a"
-alias m="mutt -F $HOME/.config/mutt/muttrc && mbsync -a"
+alias mutt="mutt -F $HOME/.config/mutt/muttrc && mbsync -qa &"
+alias m="mutt -F $HOME/.config/mutt/muttrc && mbsync -qa &"
 alias h="history"
 alias py="python"
 alias .vim="cd $HOME/.vim && ls"
