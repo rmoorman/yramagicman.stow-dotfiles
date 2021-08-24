bc1675c28c7d5e85aa954dac7c26f4ffa89443af
Jonathan <jonathandavis@gilsons.org>
2021-08-24 09:24:10 -0400

    disallow reboot and clean temp on reboot

--- nixos/header.nix
+++ nixos/header.nix
@@ -6,16 +6,17 @@
 {
     system.autoUpgrade.enable = true;
     system.autoUpgrade.dates = "18:00";
-    system.autoUpgrade.allowReboot = true;
     nix.gc.automatic = true;
     nix.gc.dates = "18:30";
     nix.gc.options = "--delete-older-than 14d";
     nix.autoOptimiseStore = true;
 
+
     # Use the systemd-boot EFI boot loader.
     boot.loader.systemd-boot.configurationLimit = 15;
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
+    boot.cleanTmpDir = true;
 
     # Set your time zone.
     time.timeZone = "America/New_York";
@@ -23,8 +24,8 @@
     # Select internationalisation properties.
     i18n.defaultLocale = "en_US.UTF-8";
     console = {
-      font = "Lat2-Terminus16";
-      keyMap = "us";
+        font = "Lat2-Terminus16";
+        keyMap = "us";
     };
 
     swapDevices = [
