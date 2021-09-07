8030aff3453b25eeb3a2bf720b5b6fb0e9d685a0
Jonathan <jonathandavis@gilsons.org>
2021-09-07 16:34:07 -0400

    don't sleep on f12

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -307,7 +307,6 @@ main = do
           , ((0, xK_Print), spawn "~/.config/dwm/scripts/screenshot")
           , ((0, xK_Pause), spawn "dnd")
           , ((shiftMask, xK_F12), spawn "systemctl poweroff")
-          , ((0, xK_F12), spawn "systemctl suspend")
           , ((mod4Mask, xK_m), spawn "firefox --new-tab about:blank")
           , ((mod4Mask .|. controlMask, xK_m), spawn "firefox --new-window about:blank")
           , ((mod4Mask .|. shiftMask, xK_m), spawn "firefox --private-window")
