d1763fa86299d954c200e4a7bb35702542acae0d
Jonathan <jonathandavis@gilsons.org>
2021-12-02 16:58:31 -0500

    try to stop those interfaces from eating boot time

--- nixos/network.browncoat.nix
+++ nixos/network.browncoat.nix
@@ -1,12 +1,12 @@
 {
     networking.hostName = "browncoat"; # Define your hostname.
-      # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    networking.wireless.enable = false;  # Enables wireless support via wpa_supplicant.
 
-      # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-      # Per-interface useDHCP will be mandatory in the future, so this generated config
-      # replicates the default behaviour.
-      networking.useDHCP = false;
-      networking.interfaces.enp0s31f6.useDHCP = true;
-      networking.interfaces.enp5s0.useDHCP = false;
-      networking.interfaces.wlp6s0.useDHCP = false;
-  }
+    # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+    # Per-interface useDHCP will be mandatory in the future, so this generated config
+    # replicates the default behaviour.
+    networking.useDHCP = false;
+    networking.interfaces.enp0s31f6.useDHCP = true;
+    networking.interfaces.enp5s0.ipAddress = "127.0.0.10";
+    networking.interfaces.wlp6s0.useDHCP = "127.0.0.11";
+}
