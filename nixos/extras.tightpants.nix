5bcd28605f5fae9d6186dbd467a84648092c0489
Jonathan <jonathandavis@gilsons.org>
2021-12-10 23:50:55 -0500

    Yay, auto login on the FDE machine

--- nixos/extras.tightpants.nix
+++ nixos/extras.tightpants.nix
@@ -3,8 +3,11 @@
     services.xserver.dpi = 192;
     hardware.acpilight.enable = true;
     boot.kernelPackages = pkgs.linuxPackages_zen;
+    environment.systemPackages = with pkgs; [
+      dropbox-cli
+    ];
 
-      environment.systemPackages = with pkgs; [
-          dropbox-cli
-      ];
+    services.xserver.displayManager.autoLogin.user = "jonathan";
+    services.xserver.displayManager.autoLogin.enable = true;
+    services.xserver.displayManager.defaultSession ="none+xmonad";
 }
