127b58e470d2d1e32b84419eac0a82053c5c29a7
Jonathan <jonathandavis@gilsons.org>
2022-01-07 21:12:01 -0500

    don't auto sync mail

--- config/zsh/aliases/aliases.zsh
+++ config/zsh/aliases/aliases.zsh
@@ -9,8 +9,8 @@ alias ~="cd ~"
 alias d="dirs -v"
 #}}}
 #{{{ shortcuts
-alias mutt="mutt -F $HOME/.config/mutt/muttrc && mbsync -a"
-alias m="mutt -F $HOME/.config/mutt/muttrc && mbsync -a"
+alias mutt="mutt -F $HOME/.config/mutt/muttrc"
+alias m="mutt -F $HOME/.config/mutt/muttrc"
 alias h="history"
 alias py="python"
 alias .vim="cd $HOME/.vim && ls"
