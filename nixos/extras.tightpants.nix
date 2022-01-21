809806c330a104819003b1b49be6d5fad7bf6616
Jonathan <jonathandavis@gilsons.org>
2022-01-21 08:51:24 -0500

    add system76 packages

--- nixos/extras.tightpants.nix
+++ nixos/extras.tightpants.nix
@@ -5,9 +5,12 @@
     boot.kernelPackages = pkgs.linuxPackages_zen;
     environment.systemPackages = with pkgs; [
       dropbox-cli
+      system76-firmware
+      linuxKernel.packages.linux_zen.system76
     ];
 
     services.xserver.displayManager.autoLogin.user = "jonathan";
     services.xserver.displayManager.autoLogin.enable = true;
     services.xserver.displayManager.defaultSession ="none+xmonad";
+    hardware.system76.enableAll = true;
 }
