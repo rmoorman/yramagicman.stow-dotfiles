0cab03137c11e6450af784537e9f648535dbac73
Jonathan <jonathandavis@gilsons.org>
2022-01-26 22:42:53 -0500

    hey! this is done enough for now

--- bin/remind.rkt
+++ bin/remind.rkt
@@ -7,19 +7,20 @@
                                   (path->string (find-system-path 'home-dir)) "/" ) 2) "/"
                           #:before-first "/"))
 
-(define remind-file (string-join (list home "/.local/tasks/remind/reminders.txt" ) ""))
+(define remind-file (string-join (list home "/.local/share/remind/reminders.txt" ) ""))
 
-(define (make-partial-path path offset)
-  (let ([list-path (string-split path "/")])
-    (string-join (take list-path offset) "/" #:before-first "/")))
 
 (define (inc a) (+ 1 a))
 
 (define (m-now) (seconds->date (current-seconds)))
 
+(define (make-partial-path path offset)
+  (let ([list-path (string-split path "/")])
+    (string-join (take list-path offset) "/" #:before-first "/")))
+
 (define (check-path path offset)
   (let ([split-path (string-split path "/")])
-    (cond [(= (- (length split-path) 0 ) offset) #t]
+    (cond [(equal? (- (length split-path) 0 ) offset) #t]
           [(directory-exists? (make-partial-path path offset))
            (check-path path (inc offset))]
           [else (make-directory (make-partial-path path offset))])))
@@ -31,7 +32,7 @@
     ((lambda ()
        (check-path remind-file 0)
        (let ([ out-file (open-output-file remind-file) ] )
-         (display (newline) out-file))
+         (displayln "write a reminder here" out-file))
        ))))
 
 (define (m-remind-content)
@@ -46,8 +47,7 @@
   """ write list of reminders to file """
   (define out-file (open-output-file remind-file #:exists 'truncate ))
   (for-each (lambda line
-              (display (first line) out-file)
-              (newline out-file)) remind-list))
+              (displayln (first line) out-file)) remind-list))
 
 (define (parse-date date-string)
   """ return date struct for date time string in dd/mm/yyy HH:mm format"
@@ -74,12 +74,13 @@
 
 (define (interval-times reminder )
   """ get interval times for a specific reminder """
-  (get-interval-times (reminder-time reminder ) (reminder-interval reminder)))
+  (get-interval-times (reminder-time reminder) (reminder-interval reminder)))
 
 (define (parse-reminder reminder)
   """
   Format: dd/mm/yy HH:mm notification,intervals content.
   Takes a reminder of the above format and returns a reminder struct.
+  TODO: Enable repition with daily,weekly,montly,day-of-week
   """
   (define spaces (string-split reminder " "))
   (let ([content  (string-join (list-tail spaces 3) " " )  ]
@@ -113,10 +114,9 @@
   (for-each (lambda (n)
               (cond
                 [(check-time (parse-date (reminder-time n ) ))
-                 (system (string-append "notify-send 'on time " (reminder-content n) "'"))]
+                 (system (string-append "notify-send '" (reminder-content n) "'"))]
                 [else #f]))
             parsed-reminders))
-
 
 (define (m-notify-me-ahead-of-time)
   (for-each (lambda (r)
@@ -124,31 +124,58 @@
                           (cond
                             [(check-time n)
                              ((lambda ()
-                                (system (string-append "notify-send ' ahead" (reminder-content r) "'"))))]
+                                (system (string-append "notify-send '" (reminder-content r) "'"))))]
                             [else #f]))
                         (interval-times r)))
             parsed-reminders))
 
 (define (m-remind)
- (m-notify-me-on-time)
- (m-notify-me-ahead-of-time))
+  (m-notify-me-on-time)
+  (m-notify-me-ahead-of-time))
 
-(define valid-args (list "-h" "--help" "--list" "-l"))
 (define (m-get-args)
   (vector->list (current-command-line-arguments)))
 
-(define ( m-should-be-reminder )
-  (empty?
-    (filter (lambda (a)
-              (member a (get-args))) valid-args )))
+(define help-strings
+  (list
+    "Basic usage: remind dd/mm/yyyy int,int... some text"
+    ""
+    "Flags:"
+    "remind: Shows reminder for current minute, intended to be used as cron job"
+    "or systemd timer"
+    "remind -h, --help: Show this message."
+    "remind -l, --list: List reminders."
+    "remind -n, --next: Display notifcation with next reminder, regardless of schedule."
+    "remind -e, --edit: Open reminder list in $EDITOR."
+    ""
+    "Further Instruction:"
+    "Add a reminder with `remind dd/mm/yyyy int,int some text`."
+    "Where `int,int` is a comma separated list of any length of minutes prior to"
+    "an event at which you want to be notified"
+    ""
+    (string-append "Reminders are stored in " remind-file)))
 
-(if (and (m-should-be-reminder) (>= (length (get-args)) 3))
-  (parse-reminder (string-join (get-args)))
-  (dispatch-args (get-args)))
+(define (dispatch-args args)
+  (for-each (lambda (arg)
+              (cond
+                [(or (equal? arg "-h") (equal? arg "--help"))
+                 (map displayln help-strings)]
+                [(or (equal? arg "-l") (equal? arg "--list"))
+                 (map displayln remind-list)]
+                [(or (equal? arg "-n") (equal? arg "--next"))
+                 ;; TODO Parse out reminder times to figure out which one is
+                 ;; actually next, instead of just displaying the last reminder
+                 ;; in the file
+                 (system (string-append "notify-send '"
+                                        (reminder-content (parse-reminder
+                                                            (first (reverse remind-list)))) "'"))]
 
-;; (cond
-;;   [(= 1 (length (vector->list (current-command-line-arguments))))
-;;    (write-alacritty (first (vector->list (current-command-line-arguments))))
-;;    (write-tmp-xresources (first (vector->list (current-command-line-arguments))))
-;;    (write-config.h-tmp (first (vector->list (current-command-line-arguments))))]
-;;   [else (list-options) ])
+                [(or (equal? arg "-e") (equal? arg "--edit"))
+                 (system  (string-append "$EDITOR " remind-file))]
+                [(not (equal? 0 (length (m-get-args))))
+                  ((lambda ()
+                     (write-reminders
+                       (flatten (list remind-list (list (string-join (m-get-args))))))))]
+                [else (m-remind)])) args ))
+
+(dispatch-args (m-get-args))
