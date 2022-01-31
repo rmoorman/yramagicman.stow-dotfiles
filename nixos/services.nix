f71b88446d5a969b3b729272f0c0dd7e51651dc3
Jonathan <jonathandavis@gilsons.org>
2022-01-31 13:07:34 -0500

    refactor services

--- nixos/services.nix
+++ nixos/services.nix
@@ -1,58 +1,62 @@
 { config, pkgs, ... }:
 {
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
 
-  # Enable the GNOME Desktop Environment.
-  # services.xserver.displayManager.gdm.enable = true;
-  services.xserver.windowManager.awesome.enable = true;
-  services.xserver.windowManager.xmonad.enable = true;
-  services.xserver.windowManager.xmonad.enableContribAndExtras = true;
+      # Enable sound.
+      sound.enable = true;
+      services = {
+      # Enable the X11 windowing system.
+      xserver.enable = true;
 
-  # Configure keymap in X11
-  services.xserver.layout = "us";
-  services.xserver.xkbOptions = "caps:none";
+      # Enable the GNOME Desktop Environment.
+      # services.xserver.displayManager.gdm.enable = true;
+      xserver.windowManager.awesome.enable = true;
+      xserver.windowManager.xmonad.enable = true;
+      xserver.windowManager.xmonad.enableContribAndExtras = true;
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  services.xserver.libinput.enable = true;
-  services.xserver.libinput.touchpad.naturalScrolling = true;
-  services.xserver.libinput.touchpad.disableWhileTyping = true;
-  services.xserver.libinput.touchpad.accelSpeed = "0.25";
-  services.xserver.libinput.mouse.naturalScrolling = true;
+      # Configure keymap in X11
+      xserver.layout = "us";
+      xserver.xkbOptions = "caps:none";
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-  services.avahi.enable = true;
+      # Enable touchpad support (enabled default in most desktopManager).
+      xserver.libinput.enable = true;
+      xserver.libinput.touchpad.naturalScrolling = true;
+      xserver.libinput.touchpad.disableWhileTyping = true;
+      xserver.libinput.touchpad.accelSpeed = "0.25";
+      xserver.libinput.mouse.naturalScrolling = true;
 
-  # Enable sound.
-  sound.enable = true;
-  # hardware.pulseaudio.enable = true;
-  services.pipewire.enable = true;
-  services.pipewire.pulse.enable = true;
-  services.pipewire.alsa.enable = true;
-  services.fwupd.enable = true;
-  # services.locate.enable = true;
-  # services.locate.interval = "*:0/15";
-  services.tlp.enable = true;
+      # Enable CUPS to print documents.
+      printing.enable = true;
+      avahi.enable = true;
 
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-  services.httpd.enable = true;
-  services.httpd.adminAddr = "yramagicman@gmail.com";
-  services.httpd.enablePHP = true;
-  services.mysql.package = pkgs.mariadb;
-  services.mysql.enable = true;
+      # hardware.pulseaudio.enable = true;
+      pipewire.enable = true;
+      pipewire.pulse.enable = true;
+      pipewire.alsa.enable = true;
+      fwupd.enable = true;
+      # services.locate.enable = true;
+      # services.locate.interval = "*:0/15";
+      tlp.enable = true;
+
+      # Enable the OpenSSH daemon.
+      openssh.enable = true;
+      httpd.enable = true;
+      httpd.adminAddr = "yramagicman@gmail.com";
+      httpd.enablePHP = true;
+      mysql.package = pkgs.mariadb;
+      mysql.enable = true;
 
 
-  services.phpfpm.phpOptions = ''
-      memory_limit = 2048M
-  '';
+      phpfpm.phpOptions = ''
+          memory_limit = 2048M
+      '';
+
+      redshift.enable = true;
+      geoclue2.enable = true;
+      fstrim.enable = true;
+      atd.enable = true;
+  };
 
   virtualisation.libvirtd.enable = true;
 
   location.provider = "geoclue2";
-  services.redshift.enable = true;
-  services.geoclue2.enable = true;
-  services.fstrim.enable = true;
-  services.atd.enable = true;
 }
