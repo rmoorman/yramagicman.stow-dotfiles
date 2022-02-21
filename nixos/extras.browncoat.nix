646f980f4f50ecac93fa52e24a6b23409a39f55d
Jonathan <jonathandavis@gilsons.org>
2022-02-21 17:00:57 -0500

    hopefully fix the dumb lockup caused by gdm

--- nixos/extras.browncoat.nix
+++ nixos/extras.browncoat.nix
@@ -1,6 +1,7 @@
 { config, pkgs, ... }:
 {
     services.xserver.displayManager.gdm.enable = true;
+    services.xserver.displayManager.gdm.autoSuspend = false;
     # Mount secondary drive
     fileSystems."/home/jonathan/Storage".device = "/dev/disk/by-partuuid/22f96431-a0a9-4524-97fa-3672b86bef5d";
     boot.kernelPackages = pkgs.linuxPackages_zen;
