5c14b66ea61c6677a3e334bac5cc12161f1e0767
Jonathan <jonathandavis@gilsons.org>
2022-03-03 20:31:41 -0500

    enable gdm, disable autologin

--- nixos/extras.tightpants.nix
+++ nixos/extras.tightpants.nix
@@ -1,6 +1,6 @@
 { config, pkgs, ... }:
 {
-    services.xserver.displayManager.lightdm.enable = true;
+    services.xserver.displayManager.gdm.enable = true;
     services.xserver.dpi = 192;
     hardware.acpilight.enable = true;
     boot.kernelPackages = pkgs.linuxPackages_zen;
@@ -10,8 +10,5 @@
       linuxKernel.packages.linux_zen.system76
     ];
 
-    services.xserver.displayManager.autoLogin.user = "jonathan";
-    services.xserver.displayManager.autoLogin.enable = true;
-    services.xserver.displayManager.defaultSession ="none+xmonad";
     hardware.system76.enableAll = true;
 }
