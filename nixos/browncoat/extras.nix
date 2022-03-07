923db8ec2299c43c89e087d2457dc7ff6f4397e4
Jonathan <jonathandavis@gilsons.org>
2022-03-06 22:56:47 -0500

    Hopefully this works eventually

--- nixos/browncoat/extras.nix
+++ nixos/browncoat/extras.nix
@@ -6,11 +6,12 @@
     services.xserver.displayManager.gdm.enable = true;
     services.xserver.displayManager.gdm.autoSuspend = false;
     # Mount secondary drive
-    fileSystems."/home/jonathan/Storage".device = "/dev/disk/by-partuuid/22f96431-a0a9-4524-97fa-3672b86bef5d";
+    fileSystems."/home/jonathan/Storage".device = "/dev/disk/by-label/storage";
     boot.kernelPackages = pkgs.linuxPackages_zen;
 
     environment.systemPackages = with pkgs; [
         dropbox
+        btrfs-progs
     ];
     services.flatpak.enable = true;
     xdg.portal.enable = true;
