3361ec1f81fd8882fe30ecdc91ec7c55a19645c6
Jonathan <jonathandavis@gilsons.org>
2022-03-06 22:29:57 -0500

    Hopefully this flake thing works out, so far it's really nice.

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -3,20 +3,48 @@
 
     outputs = { self, nixpkgs }: {
 
-    # replace 'joes-desktop' with your hostname here.
-    nixosConfigurations."tightpants" = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-            ./tightpants/hardware-configuration.nix
-            ./fonts.nix
-            ./header.nix
-            ./packages.nix
-            ./services.nix
-            ./tightpants/extras.nix
-            ./tightpants/network.nix
-            ./users.nix
-        ];
+        nixosConfigurations."tightpants" = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [
+                ./tightpants/hardware-configuration.nix
+                ./fonts.nix
+                ./header.nix
+                ./packages.nix
+                ./services.nix
+                ./tightpants/extras.nix
+                ./tightpants/network.nix
+                ./users.nix
+            ];
+        };
+
+        nixosConfigurations."jayne" = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [
+                ./jayne/hardware-configuration.nix
+                ./fonts.nix
+                ./header.nix
+                ./packages.nix
+                ./services.nix
+                ./jayne/extras.nix
+                ./jayne/network.nix
+                ./users.nix
+            ];
+        };
+
+        nixosConfigurations."browncoat" = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [
+                ./browncoat/hardware-configuration.nix
+                ./fonts.nix
+                ./header.nix
+                ./packages.nix
+                ./services.nix
+                ./browncoat/extras.nix
+                ./browncoat/network.nix
+                ./users.nix
+            ];
+        };
+
     };
-};
 
 }
