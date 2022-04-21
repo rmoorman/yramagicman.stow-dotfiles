f6c06a26ce24555f6b0d680175d6d79c8c0fd82b
Jonathan <jonathandavis@gilsons.org>
2022-04-20 21:37:59 -0400

    I'll have to test this tomorrow

--- nixos/jayne/network.nix
+++ nixos/jayne/network.nix
@@ -9,9 +9,9 @@
     # Per-interface useDHCP will be mandatory in the future, so this generated config
     # replicates the default behaviour.
     networking.useDHCP = false;
-    networking.interfaces.enp1s0.useDHCP = true;
+    # networking.interfaces.enp1s0.useDHCP = true;
     # networking.interfaces.wlp2s0.useDHCP = true;
-    networking.interfaces.wlan0.useDHCP = true;
+    # networking.interfaces.wlan0.useDHCP = true;
 
     # Configure network proxy if necessary
     # networking.proxy.default = "http://user:password@proxy:port/";
@@ -27,5 +27,26 @@
     #     fsType="nfs";
     #     options = ["x-systemd.automount" "noauto"];
     # };
+
+    systemd.network = {
+        enable = true;
+        networks = {
+            "25-wireless" = {
+                dns = ["1.1.1.1" "1.0.0.1"];
+                matchConfig = { Name = "wlan0"; };
+                DHCP= "yes";
+            };
+
+            "26-wireless" = {
+                dns = ["1.1.1.1" "1.0.0.1"];
+                matchConfig = { Name = "enp1s0"; };
+                DHCP= "yes";
+            };
+
+
+        };
+    };
+
+
 
 }
