954d6680ddb4560b2c299c313ce3066fb7bca2b1
Jonathan <jonathandavis@gilsons.org>
2022-04-23 00:08:55 -0400

    update for new ip address

--- nixos/jayne/network.nix
+++ nixos/jayne/network.nix
@@ -18,7 +18,7 @@
     # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
     networking.hosts = {
-        "192.168.1.196" = ["browncoat"];
+        "192.168.1.203" = ["browncoat"];
         "127.0.0.1" = ["api.dev.local" "dev.local" ];
     };
 
