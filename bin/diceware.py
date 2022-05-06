0c50efddbcbfc32c9dac90ad15eb71920d9e0873
Jonathan <jonathandavis@gilsons.org>
2022-05-06 17:24:56 -0400

    minor updates

--- bin/diceware.py
+++ bin/diceware.py
@@ -4,9 +4,11 @@ import sys
 
 if len(sys.argv) < 2:
     print('need an argument')
+    print('arg 1: word list')
+    print('arg 2: count')
     exit()
 
-words = open('/usr/share/dict/cracklib-small')
+words = open(sys.argv[-2])
 words = words.readlines()
 pw = []
 for i in range(0, int(sys.argv[-1])):
