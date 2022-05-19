22a6c0798aa64763c091ed92e8574dbd2cfd1778
Jonathan <jonathandavis@gilsons.org>
2022-05-18 21:56:27 -0400

    enable flatpak

--- nixos/kaylee/extras.nix
+++ nixos/kaylee/extras.nix
@@ -9,6 +9,15 @@
         openrgb
     ];
 
+
+    services.flatpak.enable = true;
+    services.xserver.displayManager.gdm.autoSuspend= false;
+    xdg.portal.enable = true;
+    xdg.portal.extraPortals = [
+        pkgs.xdg-desktop-portal-gnome
+    ];
+
+
     #nixpkgs.config.packageOverrides = pkgs: {
     #    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
     #};
