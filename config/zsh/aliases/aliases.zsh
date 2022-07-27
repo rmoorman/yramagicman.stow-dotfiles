3c59cdcb4f88cf6e7cda2f30f3960eb393f6ae95
Jonathan <jonathandavis@gilsons.org>
2022-07-27 14:16:22 -0400

    tailscale alias formatting

--- config/zsh/aliases/aliases.zsh
+++ config/zsh/aliases/aliases.zsh
@@ -154,9 +154,16 @@ alias storemount="sudo mount -t nfs  browncoat:/storage /home/jonathan/Storage"
 alias bdu="btrfs filesystem du"
 alias bdf="btrfs filesystem df"
 #}}}
-# tailscale
-#{{{
+
+#{{{  tailscale
 alias tsup="sudo tailscale up --operator=jonathan --reset --exit-node '100.94.223.34'"
 alias tsdown="sudo tailscale down"
-alias tsup-exit="sudo tailscale up --operator=jonathan --advertise-exit-node --accept-routes --advertise-routes=192.168.0.0/24,192.168.1.0/24"
+function tsup-exit() {
+    sudo tailscale up \
+        --operator=jonathan \
+        --advertise-exit-node \
+        --accept-routes \
+        --exit-node-allow-lan-access=true \
+        --advertise-routes=192.168.0.0/24,192.168.1.0/24
+}
 #}}}
