410186b1af7e097be71621763f552f57e35112fc
Jonathan <jonathandavis@gilsons.org>
2022-07-26 21:46:12 -0400

    ip forwarding for tailscale

--- nixos/header.nix
+++ nixos/header.nix
@@ -12,6 +12,10 @@
     boot.loader.efi.canTouchEfiVariables = true;
     boot.cleanTmpDir = true;
 
+    boot.kernel.sysctl = {
+        "net.ipv6.conf.all.forwarding" = "1";
+        "net.ipv4.ip_forward" = "1";
+    };
     # Set your time zone.
     time.timeZone = "America/New_York";
 
