d49d5388ff7c78fdaa371b03e06020450bc6c7cc
Jonathan <jonathandavis@gilsons.org>
2022-08-25 09:02:53 -0400

    use gdm

--- nixos/services.nix
+++ nixos/services.nix
@@ -11,8 +11,8 @@
             enable = true;
 
             # Enable the GNOME Desktop Environment.
-            displayManager.sddm.enable = true;
-            displayManager.sddm.theme = "elarun";
+            displayManager.gdm.enable = true;
+            # displayManager.sddm.theme = "elarun";
             displayManager.defaultSession = "none+xmonad";
             displayManager.hiddenUsers =[ "jonathan_backup" ];
             windowManager.awesome.enable = true;
