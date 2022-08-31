caa3804e1498b7f7613d40fba21528bc1f8c2e3f
Jonathan <jonathandavis@gilsons.org>
2022-08-31 14:09:47 -0400

    disable autosuspend

--- nixos/browncoat/extras.nix
+++ nixos/browncoat/extras.nix
@@ -12,7 +12,7 @@
         intel-gpu-tools
     ];
     # services.flatpak.enable = true;
-    # services.xserver.displayManager.gdm.autoSuspend= false;
+    services.xserver.displayManager.gdm.autoSuspend= false;
     # xdg.portal.enable = true;
     # xdg.portal.extraPortals = [
     #     pkgs.xdg-desktop-portal-gnome
