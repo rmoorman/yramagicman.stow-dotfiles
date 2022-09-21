5f427725685f5639351381a1740d10ea688497a9
Jonathan <jonathandavis@gilsons.org>
2022-09-21 02:52:01 -0400

    comment out dropbox

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -186,7 +186,7 @@ commands = [
            , "emacs --bg-daemon"
            , "xset r rate 250 25"
            , "xset b off"
-           , "dropbox"
+           -- , "dropbox"
            , "tmuxcopy"
            , "nextcloud --background"
            , "dunst"
