4e62983dfd8771070ab2c5b1b8f204dc6bf6ec0c
Jonathan <jonathandavis@gilsons.org>
2022-10-23 22:23:02 -0400

    comment out v4l stuff

--- nixos/kaylee/extras.nix
+++ nixos/kaylee/extras.nix
@@ -1,22 +1,22 @@
 { config, pkgs, ... }:
 {
   boot.kernelPackages = pkgs.linuxPackages_6_0;
-  boot.extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback.out ];
+  # boot.extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback.out ];
 
-  boot.kernelModules = [
-    # Virtual Camera
-    "v4l2loopback"
-    # Virtual Microphone, built-in
-    "snd-aloop"
-  ];
+  # boot.kernelModules = [
+  #   # Virtual Camera
+  #   "v4l2loopback"
+  #   # Virtual Microphone, built-in
+  #   "snd-aloop"
+  # ];
 
-  # Set initial kernel module settings
-  boot.extraModprobeConfig = ''
-    # exclusive_caps: Skype, Zoom, Teams etc. will only show device when actually streaming
-    # card_label: Name of virtual camera, how it'll show up in Skype, Zoom, Teams
-    # https://github.com/umlaeute/v4l2loopback
-    options v4l2loopback exclusive_caps=1 card_label="Virtual Camera"
-  '';
+  # # Set initial kernel module settings
+  # boot.extraModprobeConfig = ''
+  #   # exclusive_caps: Skype, Zoom, Teams etc. will only show device when actually streaming
+  #   # card_label: Name of virtual camera, how it'll show up in Skype, Zoom, Teams
+  #   # https://github.com/umlaeute/v4l2loopback
+  #   options v4l2loopback exclusive_caps=1 card_label="Virtual Camera"
+  # '';
 
   fileSystems."/".options = ["compress=zstd"];
 
@@ -26,10 +26,6 @@
   ];
 
   services.flatpak.enable = true;
-  xdg.portal.enable = true;
-  xdg.portal.extraPortals = [
-    pkgs.xdg-desktop-portal-gnome
-  ];
 
   #nixpkgs.config.packageOverrides = pkgs: {
   #    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
