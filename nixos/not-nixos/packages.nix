3c343e93a4cb149265a17ce84d54b927f343440f
Jonathan <jonathandavis@gilsons.org>
2022-12-03 17:49:14 -0500

    re-indent based on emacs nix-mode

--- nixos/not-nixos/packages.nix
+++ nixos/not-nixos/packages.nix
@@ -1,14 +1,13 @@
 { config, pkgs, lib, ... }:
 {
-
   home.packages = with pkgs; [
-        cobang
-        dbeaver
-        du-dust
-        gphoto2
-        ngrok
-        powertop
-        slack
-        sqlite
+    cobang
+    dbeaver
+    du-dust
+    gphoto2
+    ngrok
+    powertop
+    slack
+    sqlite
   ];
 }
