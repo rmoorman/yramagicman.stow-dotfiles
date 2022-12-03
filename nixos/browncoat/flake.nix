c318e39ed8d082d9e7e332bf47562beeef9ff55f
Jonathan <jonathandavis@gilsons.org>
2022-12-02 23:19:59 -0500

    browncoat now its own flake

---
+++ nixos/browncoat/flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "A very basic flake";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  outputs = inputs@{ nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      base = [
+        ../fonts.nix
+        ../header.nix
+        ../packages.nix
+        ../services.nix
+        ../users.nix
+      ];
+    in {
+
+      nixosConfigurations."browncoat" = with base; nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = base ++ [
+          ./default.nix
+          ../systemd/store-scrub.nix
+          ../systemd/store-status.nix
+          ../systemd/store-balance.nix
+        ];
+      };
+
+    };
+}
