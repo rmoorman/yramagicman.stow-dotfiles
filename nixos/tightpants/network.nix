e787362c684d76f4e3f6832e754b89950808f1fc
Jonathan <jonathandavis@gilsons.org>
2022-12-16 10:36:45 -0500

    use systemd networkd

--- nixos/tightpants/network.nix
+++ nixos/tightpants/network.nix
@@ -3,6 +3,7 @@
     # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
     networking.wireless.iwd.enable = true;  # Enables wireless support via iwd.
     networking.networkmanager.wifi.backend = "iwd";
+    networking.networkmanager.enable = false;
 
     # networking.useDHCP = true;
 
@@ -10,9 +11,9 @@
     # networking.proxy.default = "http://user:password@proxy:port/";
     # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-    networking.hosts = {
-        "192.168.1.203" = ["browncoat"];
-    };
+    # networking.hosts = {
+    #     "192.168.1.203" = ["browncoat"];
+    # };
 
     # fileSystems."/home/jonathan/Music" = {
     #     device ="browncoat:/music";
@@ -20,15 +21,15 @@
     #     options = ["x-systemd.automount" "noauto"];
     # };
 
-    # systemd.network = {
-    #     enable = true;
-    #     networks = {
-    #         "25-wireless" = {
-    #             matchConfig = { Name = "wlan0"; };
-    #             DHCP= "yes";
-    #         };
-    #     };
-    # };
+    systemd.network = {
+        enable = true;
+        networks = {
+            "25-wireless" = {
+                matchConfig = { Match = "wlan*"; };
+                DHCP= "yes";
+            };
+        };
+    };
 
     services.tailscale.port=48614;
 }
