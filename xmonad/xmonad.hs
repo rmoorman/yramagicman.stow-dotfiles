48f85a08a3df9373d4e46a3c6be903d15fec27a0
Jonathan <jonathandavis@gilsons.org>
2023-04-13 20:38:13 -0400

    use urxvt until wezterm gets sorted

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -31,7 +31,7 @@ windowCount = gets $ Just . show . length . W.integrate' . W.stack . W.workspace
 -- The preferred terminal program, which is used in a binding below and by
 -- certain contrib modules.
 --
-myTerminal      = "wz"
+myTerminal      = "urxvt"
 
 -- Whether focus follows the mouse pointer.
 myFocusFollowsMouse :: Bool
