0985ca47f623f831216d1c0acfcf87cca1c0b9dc
Jonathan <jonathandavis@gilsons.org>
2023-04-20 16:11:52 -0400

    comply with shellcheck

--- systemd/bat_notify.sh
+++ systemd/bat_notify.sh
@@ -3,6 +3,6 @@
 export PATH="/run/current-system/sw/bin/:$PATH"
 cap=$(sed 1q /tmp/bat)
 batStatus=$(tail -n1 /tmp/bat)
-if [ $cap -lt 20 ] && [ $batStatus == 'Discharging' ]; then
+if [ "$cap" -lt 20 ] && [ "$batStatus" = 'Discharging' ]; then
     notify-send 'Battery Level' "$( tr '\n' ' ' < /tmp/bat)"
 fi
