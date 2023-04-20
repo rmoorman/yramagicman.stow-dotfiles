efde6015e2d9620658be3f350769c006939b6453
Jonathan <jonathandavis@gilsons.org>
2023-04-20 15:56:43 -0400

    make things work again

--- systemd/bat.sh
+++ systemd/bat.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-
+set -e
 export PATH="/run/current-system/sw/bin/:$PATH"
-cat /sys/class/power_supply/BAT0/capacity > /tmp/bat
-cat /sys/class/power_supply/BAT0/status  >> /tmp/bat
+[ -f /sys/class/power_supply/BAT0/capacity ] && cat /sys/class/power_supply/BAT0/capacity > /tmp/bat
+[ -f /sys/class/power_supply/BAT0/status ] && cat /sys/class/power_supply/BAT0/status  >> /tmp/bat
+exit 0
