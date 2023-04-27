5b4e9a1ab45b4955ecabdc689ab65933d88e78e5
Jonathan <jonathandavis@gilsons.org>
2023-04-27 14:25:20 -0400

    auto-update browncoat?

--- nixos/browncoat/extras.nix
+++ nixos/browncoat/extras.nix
@@ -1,86 +1,97 @@
 { config, pkgs, ... }:
 {
 
+
+system.autoUpgrade = {
+	enable = true;
+	dates = "23:55";
+	flake = "${config.users.users.jonathan.home}/Documents/dots/nixos/browncoat";
+	flags = [
+		"--commit-lock-file"
+	];
+	allowReboot = true;
+};
+
   # Mount secondary drive
   fileSystems."/home/jonathan/Storage".device = "/dev/disk/by-label/storage";
   fileSystems."/home/jonathan/Storage".options = ["compress=zstd"];
 
 
   environment.systemPackages = with pkgs; [
-    btrfs-progs
-    intel-gpu-tools
-    podman-compose
-    microcodeIntel
+	btrfs-progs
+	intel-gpu-tools
+	podman-compose
+	microcodeIntel
   ];
   # services.flatpak.enable = true;
   # xdg.portal.enable = true;
   # xdg.portal.extraPortals = [
-  #     pkgs.xdg-desktop-portal-gnome
+  #		pkgs.xdg-desktop-portal-gnome
   # ];
 
   virtualisation.podman = {
-    enable = true;
-    # autoPrune.enable = true;
-    dockerCompat = true;
+	enable = true;
+	# autoPrune.enable = true;
+	dockerCompat = true;
 
-    # Required for containers under podman-compose to be able to talk to each other.
-    defaultNetwork.settings.dns_enabled = true;
+	# Required for containers under podman-compose to be able to talk to each other.
+	defaultNetwork.settings.dns_enabled = true;
   };
 
   swapDevices = [
-    {
-      device = "/swapfile";
-      size = 4096;
-    }
+	{
+	  device = "/swapfile";
+	  size = 4096;
+	}
   ];
 
   hardware.opengl = {
-    enable = true;
-    extraPackages = with pkgs; [
-      intel-media-driver # LIBVA_DRIVER_NAME=iHD
-      # vaapiIntel       # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
-      vaapiVdpau
-      libvdpau-va-gl
-    ];
+	enable = true;
+	extraPackages = with pkgs; [
+	  intel-media-driver # LIBVA_DRIVER_NAME=iHD
+	  # vaapiIntel		 # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+	  vaapiVdpau
+	  libvdpau-va-gl
+	];
   };
 
   security.sudo.extraRules = [
-    {
-      users = [ "jonathan" "tomg" ];
-      commands = [
-        { command = "/opt/receive-backup"; options = [ "NOPASSWD" ]; }
-        { command = "/opt/dad-backup"; options = [ "NOPASSWD" ]; }
-      ];
-    }
+	{
+	  users = [ "jonathan" "tomg" ];
+	  commands = [
+		{ command = "/opt/receive-backup"; options = [ "NOPASSWD" ]; }
+		{ command = "/opt/dad-backup"; options = [ "NOPASSWD" ]; }
+	  ];
+	}
   ];
 
   services.apcupsd.enable = true;
 
   users.users.tomg = {
-    isNormalUser = true;
-    home  = "/home/tomg";
-    createHome  = true;
-    extraGroups = [ "audio" "video" "kvm" "wheel" ];
-    shell = pkgs.zsh;
-    initialPassword = "v/d7xBMgrZnLO[H[W`6z:3Ru@}";
+	isNormalUser = true;
+	home  = "/home/tomg";
+	createHome	= true;
+	extraGroups = [ "audio" "video" "kvm" "wheel" ];
+	shell = pkgs.zsh;
+	initialPassword = "v/d7xBMgrZnLO[H[W`6z:3Ru@}";
   };
 
   services.nextcloud = {
-    enable = true;
-    hostName = "browncoat.local";
-    home = "/srv/storage/nextcloud/home/";
-    datadir = "/srv/storage/nextcloud/data/";
-    config.adminpassFile = "${pkgs.writeText "adminpass" "test123"}";
-    package= pkgs.nextcloud26;
-    enableBrokenCiphersForSSE = false;
+	enable = true;
+	hostName = "browncoat.local";
+	home = "/srv/storage/nextcloud/home/";
+	datadir = "/srv/storage/nextcloud/data/";
+	config.adminpassFile = "${pkgs.writeText "adminpass" "test123"}";
+	package= pkgs.nextcloud26;
+	enableBrokenCiphersForSSE = false;
 
-    config.extraTrustedDomains = [
-      "100.109.94.104"
-      "100.124.185.19"
-      "100.125.24.87"
-      "100.75.20.127"
-      "100.87.66.73"
-      "100.94.223.34"
-    ];
+	config.extraTrustedDomains = [
+	  "100.109.94.104"
+	  "100.124.185.19"
+	  "100.125.24.87"
+	  "100.75.20.127"
+	  "100.87.66.73"
+	  "100.94.223.34"
+	];
   };
 }
