b4367d76c53369b6ecd390767309dabf1d5d8225
Jonathan <jonathandavis@gilsons.org>
2023-04-29 17:00:58 -0400

    suppres some warnings

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -16,7 +16,7 @@
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 (package-initialize)
-
+(setq warning-suppress-types '((comp)))
 (require 'package)
 (defvar gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
 (add-to-list 'package-archives
