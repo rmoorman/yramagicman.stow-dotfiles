c05ddd27950746542a1e5adf08b0ab08efea5b8a
Jonathan <jonathandavis@gilsons.org>
2023-05-07 11:50:09 -0400

    set static ip for browncoat

--- nixos/browncoat/network.nix
+++ nixos/browncoat/network.nix
@@ -6,13 +6,13 @@
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
-  networking.useDHCP = true;
-  # networking.interfaces.enp4s0.ipv4.addresses = [
-  #     {
-  #         address = "192.168.1.213";
-  #         prefixLength = 24;
-  #     }
-  # ];
+  networking.useDHCP = false;
+  networking.interfaces.enp4s0.ipv4.addresses = [
+    {
+      address = "192.168.1.224";
+      prefixLength = 24;
+    }
+  ];
   # networking.interfaces.enp0s31f6.ipv4.addresses = [
   #     {
   #         address = "192.168.1.203";
