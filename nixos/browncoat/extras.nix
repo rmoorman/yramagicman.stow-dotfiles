3e3e1a5f1704f41fcbd673b42c8366bac31dc143
Jonathan <jonathandavis@gilsons.org>
2023-05-07 22:29:31 -0400

    enable media transcoding (?) in jellyfin

--- nixos/browncoat/extras.nix
+++ nixos/browncoat/extras.nix
@@ -53,14 +53,17 @@ in
       size = 4096;
     }
   ];
-
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+  };
   hardware.opengl = {
     enable = true;
     extraPackages = with pkgs; [
-      intel-media-driver # LIBVA_DRIVER_NAME=iHD
-      # vaapiIntel     # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
-      vaapiVdpau
+      intel-compute-runtime
+      intel-media-driver
       libvdpau-va-gl
+      vaapiIntel
+      vaapiVdpau
     ];
   };
 
