1164df07b5876535633f26e3697a1147bdbc6360
Jonathan <jonathandavis@gilsons.org>
2023-05-17 22:56:07 -0400

    hopefully allow pihole to be a dhcp server

--- nixos/browncoat/pihole.nix
+++ nixos/browncoat/pihole.nix
@@ -8,14 +8,22 @@
         ports = [
           "53:53/udp"
           "53:53/tcp"
+          "67:67/udp"
           "8080:80/tcp"
         ];
         volumes = [
           "/srv/pihole/etc/pihole:/etc/pihole"
           "/srv/pihole/etc/dnsmasq.d:/etc/dnsmasq.d"
         ];
+        extraOptions = [
+          "--network host"
+          "--cap-add=NET_ADMIN"
+        ];
         environment = {
           WEBPASSWORD =  "fcXC2zkU5y8zvRuFugb9k9zOoIbwkkCEXOsxdvCCwNFd3IomyLBFIVfiLz4j";
+          VIRTUAL_HOST="pi.hole" ;
+          PROXY_LOCATION="pi.hole";
+          FTLCONF_LOCAL_IPV4="0.0.0.0"
         };
       };
     };
