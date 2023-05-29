dcd34e5a3bba39bfb8ea166f7dab7e9b1d600710
Jonathan <jonathandavis@gilsons.org>
2023-05-29 15:42:50 -0400

    disable nfs

--- nixos/browncoat/nfs.nix
+++ nixos/browncoat/nfs.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 {
-  services.nfs.server.enable=true;
-  services.rpcbind.enable = true;
+  services.nfs.server.enable = false;
+  services.rpcbind.enable = false;
   services.nfs.server.exports = ''
          /srv 192.168.1.0/24(rw,sync,crossmnt,fsid=0)
          /srv/music 192.168.1.0/24(rw,sync)
@@ -10,16 +10,16 @@
          /srv/home 192.168.1.0/24(rw,sync)
      '';
 
-  networking.firewall.allowedTCPPorts = [
-    2049
-    111
-    20048
-  ];
+  # networking.firewall.allowedTCPPorts = [
+  #   2049
+  #   111
+  #   20048
+  # ];
 
-  networking.firewall.allowedUDPPorts = [
-    2049
-    111
-    20048
-  ];
+  # networking.firewall.allowedUDPPorts = [
+  #   2049
+  #   111
+  #   20048
+  # ];
 
 }
