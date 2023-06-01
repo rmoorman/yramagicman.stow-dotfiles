cdd670c0f3595b95505757a0e357c91150c382c7
Jonathan <jonathandavis@gilsons.org>
2023-05-31 23:21:31 -0400

    enable profiling support for samba

--- nixos/browncoat/samba.nix
+++ nixos/browncoat/samba.nix
@@ -13,6 +13,7 @@
             security = user
             guest account = nobody
             map to guest = bad user
+            smbd profiling support = on
         '';
     shares = {
       public = {
