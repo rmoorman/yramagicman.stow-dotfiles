488e900d6bf051e50780769a18be246b2b35a2ce
Jonathan <jonathandavis@gilsons.org>
2023-06-12 17:13:11 -0400

    remove obsolete intel driver

--- nixos/browncoat/extras.nix
+++ nixos/browncoat/extras.nix
@@ -55,14 +55,14 @@ in
   nixpkgs.config.packageOverrides = pkgs: {
     vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
   };
+
   hardware.opengl = {
     enable = true;
     extraPackages = with pkgs; [
-      intel-compute-runtime
-      intel-media-driver
-      libvdpau-va-gl
-      vaapiIntel
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      # vaapiIntel       # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
       vaapiVdpau
+      libvdpau-va-gl
     ];
   };
 
