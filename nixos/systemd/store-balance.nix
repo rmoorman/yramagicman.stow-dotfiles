fe5e4b7f6f1c62f5b009c9b558bcbb83dc46d5e6
Jonathan <jonathandavis@gilsons.org>
2023-08-08 23:41:35 -0400

    run balance montly

--- nixos/systemd/store-balance.nix
+++ nixos/systemd/store-balance.nix
@@ -3,11 +3,11 @@
   systemd.services = {
     store-balance = {
       after = [ "network-online.target" ];
-      description = "Run btrfs balance yearly";
+      description = "Run btrfs balance monthly";
       enable=true;
       script = "/run/current-system/sw/bin/btrfs balance start -dusage=50 -dlimit=2 -musage=50 -mlimit=4 /srv/storage/";
       serviceConfig.Type = "simple";
-      startAt = "*-01-15 00:00:00";
+      startAt = "*-*-15 00:00:00";
       wants = [ "network-online.target" ];
     };
   };
