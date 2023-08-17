4f7bb785564d0ac40294c591230a284742a7981e
Jonathan <jonathandavis@gilsons.org>
2023-08-17 18:03:24 -0400

    systemd unit to auto-mount private storage

--- nixos/kaylee/extras.nix
+++ nixos/kaylee/extras.nix
@@ -48,6 +48,17 @@ in
     in ["${automount_opts}"];
   };
 
+  fileSystems."/mnt/private" = {
+    device = "//192.168.1.224/jonathan";
+    fsType = "cifs";
+    options = let
+      # this line prevents hanging on network split
+      automount_opts =
+          "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,uid=1000,gid=100,credentials=/tmp/creds";
+
+    in ["${automount_opts}"];
+  };
+
   programs.dconf.enable = true;
   services.flatpak.enable = true;
   services.syncthing = {
